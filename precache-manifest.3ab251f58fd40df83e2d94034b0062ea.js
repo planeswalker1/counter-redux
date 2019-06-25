c322cf746af290e547acd0b0d7ca192202d71335
Daniel Munoz <daniel.munozdev@gmail.com>
2019-06-24 22:34:22 -0700

    Updates

---
+++ precache-manifest.3ab251f58fd40df83e2d94034b0062ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e97f0d72237d651d160a0a454ebe9a",
+    "url": "/counter-redux/index.html"
+  },
+  {
+    "revision": "be817c45548a86f973e0",
+    "url": "/counter-redux/static/css/main.6406f871.chunk.css"
+  },
+  {
+    "revision": "688bf4df890b8fd9434a",
+    "url": "/counter-redux/static/js/2.52ebebe9.chunk.js"
+  },
+  {
+    "revision": "be817c45548a86f973e0",
+    "url": "/counter-redux/static/js/main.78e46037.chunk.js"
+  },
+  {
+    "revision": "b8cdc8059e998f9c0995",
+    "url": "/counter-redux/static/js/runtime~main.f5b2946d.js"
+  }
+]);
